c4fc2f1135442c48b696e5323e484d223c71766f
IsmailDX <ismaildxx@gmail.com>
2023-12-22 17:46:20 +0400

    testing meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,26 @@ import SectionTwo from "@/components/SectionTwo";
 import SwiperComponent from "@/components/SwiperComponent";
 import Filter from "@/components/Filter";
 import React from "react";
+import Head from "next/head";
 
 const Home = () => {
   return (
     <>
-      <title>Aldar Home Page</title>
+      <Head>
+        <title>Aldar Home Page</title>
+        <meta
+          name="description"
+          content="Checkout our amazing deals for properties"
+          key="desc"
+        />
+        <meta property="og:title" content="Aldar Website for properties" />
+        <meta
+          property="og:description"
+          content="Check out the best properties with aldar properties. The best offers and greatest deals are waiting for you."
+        />
+        <meta property="og:image" content="/images/aldar-logo.png" />
+      </Head>
+
       <SwiperComponent />
       <SectionTwo />
       <Filter />
