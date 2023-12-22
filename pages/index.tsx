2ba9aad2bb55371e88b4f8c6897a26aea19cfc36
IsmailDX <ismaildxx@gmail.com>
2023-12-22 17:49:33 +0400

    finishing meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,12 +14,18 @@ const Home = () => {
           content="Checkout our amazing deals for properties"
           key="desc"
         />
-        <meta property="og:title" content="Aldar Website for properties" />
+        <meta
+          property="og:title"
+          content="Aldar Website for properties by IsmailDX"
+        />
         <meta
           property="og:description"
           content="Check out the best properties with aldar properties. The best offers and greatest deals are waiting for you."
         />
-        <meta property="og:image" content="/images/aldar-logo.png" />
+        <meta
+          property="og:image"
+          content="https://cdn.aldar.com/-/media/project/aldar-tenant/aldar2/images/brandhub/logo-black.svg?rev=a1b383f772e94d4d82d58562d52a81a5&extension="
+        />
       </Head>
 
       <SwiperComponent />
